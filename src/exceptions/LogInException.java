6aa1c29aa0b6d86fa7e87ddae588aa426b0102ca
tarasula <andreyorlov86@gmail.com>
2017-02-22 22:14:48 +0200

    Update LogInException.java

--- src/exceptions/LogInException.java
+++ src/exceptions/LogInException.java
@@ -11,35 +11,25 @@ public class LogInException extends Exception{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private String message;
 	
 	public LogInException(ClientType type){
 		switch (type) {
 		case CUSTOMER:
-			this.message = CustomerConstants.LOGIN_FAILED;
+			super(CustomerConstants.LOGIN_FAILED);
 			break;
 		case COMPANY:
-			this.message = CompanyConstants.LOGIN_FAILED;
+			super(CompanyConstants.LOGIN_FAILED);
 			break;
 		case ADMINISTRATOR:
-			this.message = AdminConstants.LOGIN_FAILED;
+			super(AdminConstants.LOGIN_FAILED);
 			break;
 		case ADMIN:
-			this.message = AdminConstants.LOGIN_FAILED;
+			super(AdminConstants.LOGIN_FAILED);
 			break;
 		}
 	}
 	
 	public LogInException(String message){
-		this.message = message;
-	}
-	
-	/**
-	 * (non-Javadoc)
-	 * @see java.lang.Throwable#getMessage()
-	 */
-	@Override
-	public String getMessage() {
-		return message;
+		super(message);
 	}
 }
